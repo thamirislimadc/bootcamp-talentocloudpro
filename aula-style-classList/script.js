697786870f72d6de3abacb105e4e221bbe110a6c
ThamirisLimaDC <thamirislimadc@gmail.com>
2024-07-22 21:32:57 -0300

    Exercício JS

---
+++ aula-style-classList/script.js
@@ -0,0 +1,12 @@
+let h2 = document.querySelector('h2');
+h2.style.color = 'red';
+h2.style.fontSize = '50px';
+
+let botao = document.querySelector('button');
+botao.style.backgroundColor = 'blue';
+botao.style.color = 'white';
+botao.style.fontSize = '14px';
+botao.style.padding = '10px';
+botao.style.border = 'none';
+botao.style.borderRadius = '5px';
+botao.style.cursor = 'pointer';
